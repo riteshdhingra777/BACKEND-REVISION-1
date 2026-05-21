9df8a16e28c7e2452402f23526f85a3e93eeb824
riteshdhingra777 <riteshdhingra777@gmail.com>
2026-05-21 17:05:10 +0530

    controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
-import { asynchandler } from '../utils/asynchandler.js'
+import { asyncHandler } from '../utils/asynchandler.js'
 
-const registeruser = asynchandler(async (req, res) => {
+const registeruser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "user registered successfully" })
 })
+
+
+
 
 export { registeruser }
